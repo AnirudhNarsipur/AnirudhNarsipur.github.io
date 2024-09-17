a94932787bf7fef0ac1c8d0dc11ead2678613d9e
Anirudh Narsipur <anirudh.narsipur@gmail.com>
2024-09-16 22:59:08 -0400

    deploy only on dispatch

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://anirudhnarsipur.github.io/',
+    site: 'https://anarsipur.com',
     output: "static",
     integrations: [
         mdx(),
